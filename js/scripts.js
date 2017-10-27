5e2cb953f0b62584a8f08d8ef972a3dbc465b7ab
Łukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-10-27 20:07:40 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Pole Trójkąta
+var a = prompt('wpisz długość podstawy'),
+	h = prompt('wpisz wysokość'),
+	triangleArea = a*h/2;
+alert('Pole trójkąta wynosi o podstawie a: ' + a + " i wysokości h: " + h + " jest równe: " + triangleArea);
+console.log('Pole trójkąta wynosi o podstawie a: ' + a + " i wysokości h: " + h + " jest równe: " + triangleArea);
+	
+
