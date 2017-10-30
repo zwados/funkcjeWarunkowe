cb08928957d3acd4d77cf417f7e9d60627b89812
Łukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-10-30 23:36:25 +0100

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-// Pole Trójkąta
-var a = prompt('wpisz długość podstawy'),
-	h = prompt('wpisz wysokość'),
-	triangleArea = a*h/2;
-alert('Pole trójkąta wynosi o podstawie a: ' + a + " i wysokości h: " + h + " jest równe: " + triangleArea);
-console.log('Pole trójkąta wynosi o podstawie a: ' + a + " i wysokości h: " + h + " jest równe: " + triangleArea);
+// Funkcje warunkowe
+var a = prompt('wpisz wartość "a"'),
+	b = prompt('wpisz wartość "b"'),
+	value = (a * a) - (2 * a * b) + (b * b);
+if (value > 0) {
+	alert('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość dodatnią ");
+	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value);
+		
+} else if (value < 0) {
+	alert('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość ujemną ");
+	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value);
 	
+} else
+	alert('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + " i ma wartość równą 0 ");
+	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value);
+
 
