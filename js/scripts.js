e380327816769ad0a58fd1c2fcb8f3012c4a1612
Łukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-10-31 20:37:29 +0100

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,15 @@ var a = prompt('wpisz wartość "a"'),
 	value = (a * a) - (2 * a * b) + (b * b);
 if (value > 0) {
 	alert('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość dodatnią ");
-	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value);
+	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość dodatnią ");
 		
 } else if (value < 0) {
 	alert('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość ujemną ");
-	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value);
+	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość ujemną ");
 	
-} else
+} else {
 	alert('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + " i ma wartość równą 0 ");
-	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value);
+	console.log('Dla wartości a = ' + a + " oraz wartości b = " + b + " działanie jest równe: " + value + " i ma wartość równą zero ");
+}
 
 
